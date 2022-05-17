6624498b0ffb7340780c12fc6ba320d3ed33a29b
Helge <gelovottoz@gmail.com>
2022-05-17 20:31:52 +0300

    Добавлена карта на страницу Location.html+css+ js добавлен код  .  Добавлена контактная форма Contact.html +css

--- JS/script.js
+++ JS/script.js
@@ -8,3 +8,20 @@ window.onload = function () {
     }
     }
 };
+function init(){ //location карта yandex
+    let map = new ymaps.Map('map',{
+        center:[59.91757334340399,30.41670138036452],
+        zoom: 16,
+    });
+    // удаляем
+        map.controls.remove('geolocationControl'); // геолокацию
+        map.controls.remove('searchControl'); // поиск
+        map.controls.remove('trafficControl'); //  контроль трафика
+        map.controls.remove('typeSelector'); // тип
+        map.controls.remove('fullscreenControl'); //  кнопку перехода в полноэкранный режим
+        map.controls.remove('zoomControl'); //  контрол зуммирования
+        map.controls.remove('rulerControl'); //  контрол правил
+        //map.behaviors.disable(['scrollZoom']);  скролл карты 
+}
+
+ymaps.ready(init);
